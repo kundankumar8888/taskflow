4f9233ff707d6ab02841fb8968142b5041f29593
kundankumar8888 <kundankumar97779@gmail.com>
2026-01-22 18:07:25 +0530

    handsake issue

--- backend/server.py
+++ backend/server.py
@@ -14,13 +14,14 @@ import bcrypt
 import jwt
 import stripe
 import asyncio
+import certifi
 
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
 mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(mongo_url)
+client = AsyncIOMotorClient(mongo_url, tlsCAFile=certifi.where())
 db = client[os.environ['DB_NAME']]
 
 # JWT Configuration
